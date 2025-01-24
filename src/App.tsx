fd0d2606f59b3e5d20ad0d58aa742e21dc046ccc
amarNrddn <amarnrddn02@gmail.com>
2025-01-25 00:14:24 +0700

    update(Togle menu): update hover icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react"
 import { checkThemePreference } from "./components/theme/theme"
-import ToggleTheme from "./components/theme/ToggleTheme"
 import Layout from "./pages/Layout"
 
 const App = () => {
--- src/components/navMobile/Toggle.tsx
+++ src/components/navMobile/Toggle.tsx
@@ -17,7 +17,7 @@ const Toggle = ({ isOpen, setIsOpen }: ToggleProps) => {
                   onClick={() => setIsOpen(!isOpen)}
                />
                <RiMenu2Fill
-                  className={`text-2xl font-bold text-black w-9 h-9 bg-gray-100 py-2 rounded-full ${darkkMode.bgDark} dark:bg-[#1b1b1b]`}
+                  className={`text-2xl hover:cursor-pointer font-bold text-black w-9 h-9 bg-gray-100 py-2 rounded-full ${darkkMode.bgDark} dark:bg-[#1b1b1b]`}
                   onClick={() => setIsOpen(!isOpen)}
                />
             </div>
